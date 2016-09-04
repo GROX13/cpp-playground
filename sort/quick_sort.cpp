d774fea413340a419ba08c43c3fe22fff1841c14
Giorgi <giorgirokhadze@gmail.com>
2016-09-04 13:53:50 +0400

    Quick sort algorythm finished

--- sort/quick_sort.cpp
+++ sort/quick_sort.cpp
@@ -1,12 +1,39 @@
 // Quick Sort
 #include <iostream>
+#include "utils.h"
 #include "quick_sort.h"
 
 using namespace std;
 
+void quicksort(const Array* elemnets, const int lo, const int hi);
 
+int partition(const Array* elemnets, const int lo, const int hi);
 
 Array QuickSort::sort(const Array elemnets) {
-
+	quicksort(&elemnets, 0, elemnets.size - 1);
 	return elemnets;
 }
+
+
+void quicksort(const Array* elemnets, const int lo, const int hi) {
+	if (lo < hi) {
+		const int p = partition(elemnets, lo, hi);
+		quicksort(elemnets, lo, p - 1);
+		quicksort(elemnets, p + 1, hi);
+	}
+}
+
+int partition(const Array* elemnets, const int lo, const int hi) {
+	int* A = elemnets->elements;
+	const int pivot = A[hi];
+	
+	int i = lo;
+	for (int j = lo; j < hi; j++) {
+		if (A[j] <= pivot) {
+			SwitchUtils::switchElements(A, i++, j);
+		}
+	}
+	SwitchUtils::switchElements(A, i, hi);
+	return i;
+}
+
--- sort/sort_test.cpp
+++ sort/sort_test.cpp
@@ -6,17 +6,25 @@ using namespace std;
 
 int main()
 {
-   QuickSort quickSort;
+	QuickSort quickSort;
 
-   int elems[5] = {1, 4, 6, 7, 3};
+	int elems0[6] = {1, 4, 6, 7, 3, 6};
 
-   Array elements;
+	Array elements;
 
-   elements.size = 5;
-   elements.elements = elems;
+	elements.size = 6;
+	elements.elements = elems0;
 
-   Utils::printInline("QuickSort: ");
-   Utils::printArray(quickSort.sort(elements));
-   
-   return 0;
+	Utils::printInline("QuickSort: ");
+	Utils::printArray(quickSort.sort(elements));
+
+	int elems1[17] = {1, 4, 6, 7, 3, 14, 38, 24, 90, 200, 11, 7, 9, 9, 4, 66, 27};
+
+	elements.size = 17;
+	elements.elements = elems1;
+
+	Utils::printInline("QuickSort: ");
+	Utils::printArray(quickSort.sort(elements));
+
+	return 0;
 }
